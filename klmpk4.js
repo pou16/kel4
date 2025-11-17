cbaeabeb858d442a6a7caf91b1c00f66454caaee
pou16 <alifaaprilyanti@gmail.com>
2025-11-17 08:39:12 +0700

    Update klmpk4.js

--- klmpk4.js
+++ klmpk4.js
@@ -17,3 +17,32 @@ for (let i = 1; i < tr.length; i++) {
       }
     }
   }
+function myFunction() {
+  let input = document.getElementById("myInput");
+  let filter = input.value.toUpperCase();
+  let table = document.getElementById("myTable");
+  let tr = table.getElementsByTagName("tr");
+
+  let ditemukan = false;
+
+for (let i = 1; i < tr.length; i++) {
+    let td = tr[i].getElementsByTagName("td")[0];
+
+    if (td) {
+      let txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+        ditemukan = true;
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+let pesan= document.getElementById("pesan");
+if (!ditemukan) {
+  pesan.style.display = "block";
+  console.log("Data Tidak Ditemukan");
+} else {
+  pesan.style.display = "none";
+}
+}
